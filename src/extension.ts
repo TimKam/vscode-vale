5123e5943bd5736ce66b7ebdfd341028690e423b
Timotheus Kampik <timotheus.kampik@gmail.com>
2018-02-26 23:40:39 +0100

    Add support for additional markup languages This commit adds support for all file endings Vale lints by default. One could go even further and read the list of file endings that should be considered from the vale configuration file (running "vale dc" on the command line), but I wanted to keep it simple for now

--- src/extension.ts
+++ src/extension.ts
@@ -45,8 +45,7 @@ import {
  * @return Whether the document is elligible
  */
 const isElligibleDocument = (document: TextDocument): boolean =>
-    !document.isDirty && 0 < languages.match({
-        language: "markdown",
+    !document.isDirty && 0 < languages.match({ 
         scheme: "file",
     }, document);
 
@@ -265,7 +264,7 @@ const runValeOnWorkspace = async (): Promise<ValeDiagnostics> => {
     // Explicitly find all elligible files ourselves so that we respect
     // "files.exclude", ie, only look at files that are included in the
     // workspace.
-    const extensions: ReadonlyArray<string> = ["md", "markdown"];
+    const extensions: ReadonlyArray<string> = ["md", "markdown", "txt", "rst", "tex"];
     const pattern = `**/*.{${extensions.join(",")}}`;
     const uris = await workspace.findFiles(pattern);
     const results: ValeDiagnostics = new Map();
